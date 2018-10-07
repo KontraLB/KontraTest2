965dbd33cfb7c0ed11f1d7273b7803417450340c
Oliver <oliver.cedenheim@gmail.com>
2018-10-07 17:04:23 +0200

    my latest

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ class TypeOneForm(FlaskForm):
     violation = IntegerField("Vilket övertädelsenummer gäller det?", validators=[DataRequired(), NumberRange(min=0, max=100, message="mellan 0 och 100")])
     place = RadioField('Vart fick du bötern?', choices=[('sthlm','Stockholm'),('lnkpg','Linköping')],
                         validators=[DataRequired()])
-    submit = SubmitField('Klar')
+    submit = SubmitField('Nästa')
 
 class ViolationOne(FlaskForm):
     q1 = RadioField('Har du parkerat inom tätbebyggt område som är terräng?', choices=[('yes','ja'),('no','nej')],
@@ -14,9 +14,9 @@ class ViolationOne(FlaskForm):
     q2 = RadioField('Har du parkerat på en cyckelbana eller gångbana?',
                          choices=[('jaCyckelbana', 'ja cyckelbana'), ('jaGångbana', 'Ja gångbana'), ('no', 'nej')],
                          validators=[DataRequired()])
-    submit = SubmitField('Klar')
+    submit = SubmitField('Nästa')
 
 class ViolationTwo(FlaskForm):
     q1 = RadioField('Har du stannat eller parkerat inom område där fordon inte får stannas eller parkeras?',
                      choices=[('yes', 'ja'), ('no', 'nej')])
-    submit = SubmitField('Klar')
+    submit = SubmitField('Nästa')
